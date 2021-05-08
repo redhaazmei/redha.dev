c91084cf6e99f365887e2b53e58a70b73e9b1faf
Redha Azmei <redha.azmei@gmail.com>
2021-05-09 03:55:09 +0800

    Modify Homepage

---
+++ pages/index.jsx
@@ -0,0 +1,19 @@
+import { Box, Text, useColorMode } from "@chakra-ui/react";
+import SectionHeader from "components/SectionHeader";
+import Hackathons from "components/Hacktahons";
+
+const Home = () => {
+  const { colorMode } = useColorMode();
+  return (
+    <>
+      <Box mb={"8"}>
+        <SectionHeader>About</SectionHeader>
+        <Text>Howdy 👋. I'm a Data Scientist with deep interest in Machine Learning (ML), Artificial Intelligence (AI) and modern Web Technologies. Currently working in the aviation industry based in Malaysia 🇲🇾.</Text>
+      </Box>
+      <SectionHeader>Hackathons</SectionHeader>
+      <Hackathons />
+    </>
+  );
+};
+
+export default Home;
