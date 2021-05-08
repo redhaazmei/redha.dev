6dd196f137a78f38647dd9404611fff734699520
Redha Azmei <redha.azmei@gmail.com>
2021-05-09 03:53:18 +0800

    Add Chakra Provider, custom theme and set default layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "focus-visible/dist/focus-visible";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/800.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import Layout from "components/Layout";
+import theme from "theme";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  );
+};
 
-export default MyApp
+export default App;
