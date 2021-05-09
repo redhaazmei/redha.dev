8e4913e3023a3a5e1de52bce0e03a214a512ab5b
Redha Azmei <redha.azmei@gmail.com>
2021-05-10 01:03:22 +0800

    Preconnect Inter fonts for font optimization

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import "focus-visible/dist/focus-visible";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/800.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "components/Layout";
 import theme from "theme";
