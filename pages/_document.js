2374fc8debcfa5f5bfa96405e97f7b5cc47bd88d
Redha Azmei <redha.azmei@gmail.com>
2021-05-10 05:51:30 +0800

    Modify font preload config

--- pages/_document.js
+++ pages/_document.js
@@ -9,12 +9,12 @@ export default class Document extends NextDocument {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700&display=optional" />
-          <link rel="stylesheet preload" as="style" href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700&display=optional" crossOrigin="anonymous" />
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
+          <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700&display=optional" crossOrigin="anonymous" />
         </body>
       </Html>
     );
