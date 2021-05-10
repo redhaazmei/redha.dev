ee60c8ba147e3460b12dbe65da4a1dd54ef5de74
Redha Azmei <redha.azmei@gmail.com>
2021-05-11 01:29:41 +0800

    Modify breakpoints

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Flex alignItems={"center"} justifyContent={"center"}>
-        <Flex direction={"column"} maxW={"2xl"} w={"100%"} mt={"20"} mx={"6"}>
+        <Flex direction={"column"} maxW={"2xl"} w={["xs", "md", "xl", "2xl"]} mt={"16"} mx={"6"}>
           <Navbar />
           {children}
           <Footer />
