157d3f53768170d5ffb6fb134b574c1c696d0420
Redha Azmei <redha.azmei@gmail.com>
2021-05-11 01:31:32 +0800

    Include MDXProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import "focus-visible/dist/focus-visible";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/fira-code";
+import { MDXProvider } from "@mdx-js/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "components/Layout";
+import MDXComponents from "components/MDXComponents";
 import theme from "theme";
 
 const App = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <MDXProvider components={MDXComponents}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MDXProvider>
     </ChakraProvider>
   );
 };
