e913668ec5f6330485a8ac0e28de9260500874bd
Redha Azmei <redha.azmei@gmail.com>
2021-05-11 01:32:51 +0800

    Update slug and index page of blog

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -1,15 +1,38 @@
+import Head from "next/head";
+import Link from "next/link";
 import { getallslugs, getsinglepost } from "lib/getposts";
 import { NextSeo } from "next-seo";
 import { MDXRemote } from "next-mdx-remote";
 import { serialize } from "next-mdx-remote/serialize";
+import { Box, Text, Icon, Flex, Heading } from "@chakra-ui/react";
+import { FiArrowLeft } from "react-icons/fi";
 import MDXWrapper from "components/MDXWrapper";
 
-const BlogPost = ({ mdxSource }) => {
+const BlogPost = ({ frontmatter, content }) => {
   return (
     <>
-      <div>
-        <MDXRemote {...mdxSource} />
-      </div>
+      <Head>
+        <title>{frontmatter.title} | Redha Azmei</title>
+      </Head>
+      <Link href={"/blog"}>
+        <Flex alignItems={"center"} mt={"4"} mb={"6"}>
+          <Icon as={FiArrowLeft} />
+          <Text fontWeight={"bold"} ml={"1"} _hover={{ cursor: "pointer", color: "primary" }}>
+            Back to blog
+          </Text>
+        </Flex>
+      </Link>
+      <Box mb={"8"}>
+        <Heading size={"2xl"} mb={"2"}>
+          {frontmatter.title}
+        </Heading>
+        <Text>
+          {frontmatter.date} | {frontmatter.readingtime}
+        </Text>
+      </Box>
+      <MDXWrapper>
+        <MDXRemote {...content} />
+      </MDXWrapper>
     </>
   );
 };
@@ -24,10 +47,12 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async ({ params: { slug } }) => {
   const singlepost = await getsinglepost(slug);
-  const mdxSource = await serialize(singlepost.content);
+  const frontmatter = { title: singlepost.title, date: singlepost.date, description: singlepost.description, category: singlepost.category, image: singlepost.image, slug: singlepost.slug, readingtime: singlepost.readingtime };
+  const content = await serialize(singlepost.content);
   return {
     props: {
-      mdxSource,
+      frontmatter,
+      content,
     },
   };
 };
--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,17 +1,35 @@
 import Head from "next/head";
+import Link from "next/link";
 import { getallposts } from "lib/getposts";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Heading, Text, InputGroup, InputLeftElement, Input, useColorMode } from "@chakra-ui/react";
+import { FiSearch } from "react-icons/fi";
 import SectionHeader from "components/SectionHeader";
 
 const Blog = ({ allposts }) => {
+  const { colorMode } = useColorMode();
   return (
     <>
-      <Box mb={"8"}>
-        <SectionHeader>Blog</SectionHeader>
-        <Text>
-          I've written a total of <strong>34</strong> blog posts here.
-        </Text>
-        <Text>Find the articles that suit your interests by searching in the 🔍 box below.</Text>
+      <Head>
+        <title>Blog | Redha Azmei</title>
+      </Head>
+      <SectionHeader>Blog</SectionHeader>
+      <InputGroup mb={"4"}>
+        <InputLeftElement pointerEvents={"none"} children={<FiSearch />} />
+        <Input type={"text"} variant={"flushed"} focusBorderColor={"primary"} placeholder="Search article" />
+      </InputGroup>
+      <Box>
+        {allposts.map((post) => {
+          return (
+            <Link key={post.title} href={`/blog/${post.slug}`}>
+              <Box role={"group"} py={"2"} _hover={{ cursor: "pointer" }}>
+                <Heading size={"md"} mb={"1"} _groupHover={{ color: colorMode === "dark" ? "primary" : "secondary" }}>
+                  {post.title}
+                </Heading>
+                <Text mb={"2"}>{post.description}</Text>
+              </Box>
+            </Link>
+          );
+        })}
       </Box>
     </>
   );
