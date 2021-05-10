1121ecb2a62cb493a028b42c33ab61263cd6122f
Redha Azmei <redha.azmei@gmail.com>
2021-05-10 12:10:12 +0800

    Update Blog page

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,11 +1,29 @@
+import Head from "next/head";
+import { getallposts } from "lib/getposts";
+import { Box, Text } from "@chakra-ui/react";
 import SectionHeader from "components/SectionHeader";
 
-const Blog = () => {
+const Blog = ({ allposts }) => {
   return (
     <>
-      <SectionHeader>Blog</SectionHeader>
+      <Box mb={"8"}>
+        <SectionHeader>Blog</SectionHeader>
+        <Text>
+          I've written a total of <strong>34</strong> blog posts here.
+        </Text>
+        <Text>Find the articles that suit your interests by searching in the 🔍 box below.</Text>
+      </Box>
     </>
   );
+};
+
+export const getStaticProps = async () => {
+  const allposts = getallposts();
+  return {
+    props: {
+      allposts,
+    },
+  };
 };
 
 export default Blog;
