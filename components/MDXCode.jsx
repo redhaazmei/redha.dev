d557db0f43c6da2f430407ce79aa1f5bdf7cce20
Redha Azmei <redha.azmei@gmail.com>
2021-05-12 03:04:24 +0800

    Change pre styling

--- components/MDXCode.jsx
+++ components/MDXCode.jsx
@@ -1,4 +1,4 @@
-import { useColorMode } from "@chakra-ui/react";
+import { Box, useColorMode } from "@chakra-ui/react";
 import Highlight, { defaultProps } from "prism-react-renderer";
 import nightowllight from "prism-react-renderer/themes/nightOwlLight";
 import nightowldark from "prism-react-renderer/themes/nightOwl";
@@ -8,8 +8,8 @@ const MDXCode = ({ children, className }) => {
   const language = className.replace(/language-/, "");
   return (
     <Highlight {...defaultProps} theme={colorMode === "dark" ? nightowldark : nightowllight} code={children} language={language}>
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className}>
+      {({ className, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={{ overflowX: "auto", whiteSpace: "nowrap", paddingTop: "2em", paddingLeft: "1em", scrollbarWidth: "none" }}>
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({ line, key: i })}>
               {line.map((token, key) => (
