d02980c05e483ebd2bd71b1eb229c7d7a7090cb7
Redha Azmei <redha.azmei@gmail.com>
2021-05-15 22:36:59 +0800

    Include MDXComponents

--- components/MDXComponents.jsx
+++ components/MDXComponents.jsx
@@ -1,7 +1,28 @@
+import { Box, Link, useColorModeValue } from "@chakra-ui/react";
 import MDXCode from "components/MDXCode";
+
+const MDXAnchor = ({ children, href }) => {
+  return (
+    <>
+      <Link href={href} as="a" color="secondary" fontWeight="bold" textDecor="underline" _hover={{ color: "primary" }} isExternal>
+        {children}
+      </Link>
+    </>
+  );
+};
+
+const MDXBlockquote = ({ children }) => {
+  return (
+    <Box as="blockquote" bg={useColorModeValue("gray.100", "gray.900")} borderLeft="solid" borderColor="primary" pl="6" pr="4" py="4" mb="8" fontWeight="bold">
+      {children}
+    </Box>
+  );
+};
 
 const MDXComponents = {
   code: MDXCode,
+  a: MDXAnchor,
+  blockquote: MDXBlockquote,
 };
 
 export default MDXComponents;
